6065affa6203ac01daa102ba1dc62a16b7f4cad8
AirysDark <47623717+AirysDark@users.noreply.github.com>
2026-08-27 07:22:22 +1000

    Fix OEM key backup option availability

--- WinKey/KeyBackupChoiceWindow.xaml.cs
+++ WinKey/KeyBackupChoiceWindow.xaml.cs
@@ -13,18 +13,39 @@ public partial class KeyBackupChoiceWindow : Window
 
     public KeyChoice SelectedChoice { get; private set; } = KeyChoice.None;
 
-    public KeyBackupChoiceWindow()
+    public KeyBackupChoiceWindow(bool installedKeyAvailable, bool oemKeyAvailable)
     {
         InitializeComponent();
+
+        InstalledKeyOption.IsEnabled = installedKeyAvailable;
+        OemKeyOption.IsEnabled = oemKeyAvailable;
+
+        if (installedKeyAvailable)
+        {
+            InstalledKeyOption.IsChecked = true;
+        }
+        else if (oemKeyAvailable)
+        {
+            OemKeyOption.IsChecked = true;
+        }
     }
 
     private void Continue_Click(object sender, RoutedEventArgs e)
     {
-        SelectedChoice = OemKeyOption.IsChecked == true
-            ? KeyChoice.Oem
-            : KeyChoice.Installed;
+        if (OemKeyOption.IsChecked == true && OemKeyOption.IsEnabled)
+        {
+            SelectedChoice = KeyChoice.Oem;
+        }
+        else if (InstalledKeyOption.IsChecked == true && InstalledKeyOption.IsEnabled)
+        {
+            SelectedChoice = KeyChoice.Installed;
+        }
+        else
+        {
+            SelectedChoice = KeyChoice.None;
+        }
 
-        DialogResult = true;
+        DialogResult = SelectedChoice != KeyChoice.None;
     }
 
     private void Cancel_Click(object sender, RoutedEventArgs e)
